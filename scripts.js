41694f9c2984b1f8c70b0467c215c6deef03ba4d
gabesz0909 <szg0909@gmail.com>
2020-04-30 19:20:21 +0200

    Teljes

--- scripts.js
+++ scripts.js
@@ -18,7 +18,12 @@ $('h2').toggleClass('blue');
 
 
 function mylittlefunction() {
+    //condition: number of ps is smaller than 5
+    if ($('p').length< 5) {
     $('main').append('<p>A p is appended in the main </p>');
+    }else {
+        console.log('We reached the maximum paragraph number');
+    }
     $('p').css('border','1px solid red');
 }
 
@@ -27,6 +32,24 @@ mylittlefunction();
 
 $('#add').click(mylittlefunction);
 $('#remove').click(function () {
-    $('p:last-child').remove(); }); 
+    $   ('p:last-child').remove(); }); 
+
+
+let colors=['orange','yellow','blue'];
+
+$('#box-container').append('<div class="box"></div>');
+$('.box:last-child').css('background', colors[0]);
+$('#box-container').append('<div class="box"></div>');
+$('.box:last-child').css('background', colors[1]);
+$('#box-container').append('<div class="box"></div>');
+$('.box:last-child').css('background', colors[2]);
+
+
+
+//if (Condition that true or false) {
+    //condition true
+//} else{ 
+    //condition false
+//}
 
 //$('h2').toggleClass('blue'); előző parancsot visszavonja ha 2- szer ugyanazt használjuk
